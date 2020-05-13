6f6b69fb6173ae259020fdfa343cc13bcc4fc4c8
janm84 <43088565+janm84@users.noreply.github.com>
2020-05-13 12:53:20 +0200

    Update MMM-MQTTbind.js

--- MMM-MQTTbind.js
+++ MMM-MQTTbind.js
@@ -1,28 +1,37 @@
 "use strict";
 
-// Magicmirror Modul - MMM-MQTTbind - MIT Licensed
-// Autor: Jan Mittelstaedter 
-// 
-// Originally Based on MMM-MQTTbridge (MIT-License) - https://github.com/sergge1/MMM-MQTTbridge
+// Magicmirror Modul - MMM-MQTTbind
+// by Jan Mittelstaedter
+//
+// MIT Licensed
+//
+// Please see: https://github.com/janm84/MMM-MQTTbind
+//
+// Inspired from MMM-MQTTbridge (MIT-License) - https://github.com/sergge1/MMM-MQTTbridge
 
 
 Module.register("MMM-MQTTbind", {
+
+  //Default Values for the Configuration
   defaults: {
     server: "",
     ignoredNotifications: [],
     ignoredSenders: [],
     bindings: {
       subscriptions: [],
-      publications: [],					
+      publications: [],
     },
   },
 
+  // Declaration of interal Variables
   start: function () {
     Log.info("Starting module: " + this.name);
-    this.loaded = false;
+    this.loaded = false;      // true when Connected the first time
     this.initialize(this);
+    this.startupQueue = [];   // Queue for Messages where Received before connected to MQTT Server
   },
 
+  // Provide the node helper with the configuration and trigger the connection
   initialize: function (self) {
     self.sendSocketNotification("MQTT_BIND_CONNECT", self.config);
   },
@@ -31,16 +40,31 @@ Module.register("MMM-MQTTbind", {
     var self = this;
     switch (notification) {
 
+      // Triggered by node_helper when a msg from from mqtt server was received
       case "MQTT_MESSAGE_RECEIVED": {
-          for (var i = 0; i < this.config.bindings.subscriptions.length; i++) {
-            if(this.config.bindings.subscriptions[i].topic == payload.topic) {
-              this.sendNotification(this.config.bindings.subscriptions[i].notification, payload.message);
-              break;              
-            }
+        for (var i = 0; i < this.config.bindings.subscriptions.length; i++) {
+          if(this.config.bindings.subscriptions[i].topic == payload.topic) {
+            this.sendNotification(this.config.bindings.subscriptions[i].notification, payload.message);
+            break;
           }
         }
-        break;
+      }
+      break;
 
+      // triggered by node_hellper when the connection is established
+      case "MQTT_BIND_CONNECTED": {
+        this.loaded = true;
+
+        // sending queued notifications
+        for (var i = 0; i < this.startupQueue.length; i++) {
+          let item = this.startupQueue.shift();
+          this.sendNotification(item.notification, item.payload);
+          this.log("LOG", "Publish Queued Item: " + i+1 + "(notification: " + item.notification + ", payload: " + payload + ")");
+        }
+        break;
+      }
+
+      // Triggered by node_helper in case of a Error
       case "ERROR": {
         this.sendNotification("SHOW_ALERT", payload);
         break;
@@ -52,40 +76,53 @@ Module.register("MMM-MQTTbind", {
     var self = this;
     var sndname = "system";
 
-    if (!sender === false) { sndname = sender.name; }; 
+    if (!sender === false) { sndname = sender.name; };
 
+    // Checking if the sender is in the list of ignored senders. Thoughts to change this into a whitelist.
     for (var x in this.config.ignoredSenders) {
       if (sndname == this.config.ignoredSenders[x]) { return; }
     }
 
+    // Checking if the ntification is in the list of ignored norifications. Thoughts to change this into a whitelist.
     for (var x in this.config.ignoredNotifications) {
       if (notification == this.config.ignoredNotifications[x]) { return; }
     }
 
+    // Checking if a binding exists in the configuration for this notification and forward it to node_helper.
     for (var i = 0; i < this.config.bindings.publications.length; i++) {
       if (this.config.bindings.publications[i].notification === notification) {
-        this.sendSocketNotification("MQTT_MESSAGE_SEND", { 'binding': this.config.bindings.publications[i], 'message': payload });
+
+        // If false the client is not connected and the received notification gets queued.
+        if(this.loaded = true) {
+          this.sendSocketNotification("MQTT_MESSAGE_SEND", { 'binding': this.config.bindings.publications[i], 'message': payload });
+        } else {
+          // Notifications in eine queue schreiben um sie nach dem connect abzuarbeiten.
+          this.startupQueue.push({ 'notification': notification, 'payload': payload });
+          this.log("LOG", "Adding Item to StartupQueue: (" + notification + ", payload: " + payload + ")");
+        }
         break;
       }
     }
   },
 
+  // Logging to pm2 logfile instead of browser console
   log: function(type, message) {
 
     var self = this;
-  
+
     switch(type) {
-      case 'INFO':
+
+      case 'INFO':     // Always gets logged.
         this.sendSocketNotification("LOG", { 'type': type, 'message': message});
         break;
-  
-        case 'LOG':
+
+        case 'LOG':    // Only if enable logging is true in the config.
         if(self.config.enableLogging == true) {
           this.sendSocketNotification("LOG", { 'type': type, 'message': message});
-        }           
+        }
         break;
-  
-        case 'ERROR':
+
+        case 'ERROR':   // Always gets logged
           this.sendSocketNotification("LOG", { 'type': type, 'message': message});
         break;
     }
